7e08869bd3605a32753b44929b1f1605956de9de
kirpichonok <ivankirpichonok@gmail.com>
2024-02-20 18:26:17 +0300

    Update ClientError: add description.

--- PokemonApp/Pokemon/Data/Client/ClientError.swift
+++ PokemonApp/Pokemon/Data/Client/ClientError.swift
@@ -1,8 +1,24 @@
 import Foundation
 
-enum ClientError: LocalizedError
+enum ClientError
 {
     case invalidUrl
     case networkError(NetworkError)
     case unknown(Error)
+}
+
+extension ClientError: LocalizedError
+{
+    var errorDescription: String?
+    {
+        switch self
+        {
+        case .invalidUrl:
+            return "Invalid URL address is received."
+        case let .networkError(networkError):
+            return networkError.localizedDescription
+        case let .unknown(error):
+            return "Internal error: \(error.localizedDescription)."
+        }
+    }
 }
